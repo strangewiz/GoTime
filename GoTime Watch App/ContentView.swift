409037da37afa6aa0eaee5a36ebdd57e6c2e9e77
Justin Cohen <justinsc@gmail.com>
2026-02-12 11:15:12 -0500

    Initial Commit

---
+++ GoTime Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoTime Watch App
+//
+//  Created by Justin Cohen on 2/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoTime Watch App/GoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTimeApp.swift
+//  GoTime Watch App
+//
+//  Created by Justin Cohen on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct GoTime_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
